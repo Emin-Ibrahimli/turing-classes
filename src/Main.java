6cd19398cf06f9e6007e4615f78b3f8c929d66bd
Emin Ibrahimli <eminibrahimli906@gmail.com>
2025-06-15 00:40:12 +0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int a=5;
-        int b=10;
-        System.out.println(a+b);
+        double a=5;
+        long b=7;
+        int c=3;
+        double result = (a++)+(--b)-(a++)+(a+=3)+(++c);
+        System.out.printf("result : %.2f, a: %.2f, b: %d, c: %d", result,a,b,c);
+
+
+
     }
 }
