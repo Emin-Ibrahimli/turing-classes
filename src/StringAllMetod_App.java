2cb63966a45827aaefbfeb00b7144337901cb24f
Emin Ibrahimli <eminibrahimli906@gmail.com>
2025-06-27 18:55:19 +0400

    StringAllMetod_App.java.java1

---
+++ src/StringAllMetod_App.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class StringAllMetod_App {
+    public static void main(String[] args) {
+//        Scanner scanner=new Scanner(System.in);
+//        System.out.println("Index - i daxil et: ");
+//        int n = scanner.nextInt();
+//        String deyisen = "Hello World!";
+//        System.out.println(deyisen.charAt(n));
+
+
+//
+//        Scanner scanner=new Scanner(System.in);
+//        System.out.print("Enter a string: ");
+//        String str = scanner.nextLine();
+//        System.out.println(str.toUpperCase());
+
+
+
+//    String deyisen = "HELLO WORLD";
+//        System.out.println(deyisen.toLowerCase());
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter word:");
+//        String word = scanner.nextLine();
+//        System.out.println("Word length : " + word.length());
+
+
+
+//        String str = "Hello World!";
+//        System.out.println(str.indexOf('o',5));
+
+//
+//         String str = "Hello World";
+//         str = str.replace('l','X');
+//        System.out.println(str);
+////        System.out.println(str.replace('l','X'));
+
+
+//        String str = "Hello World!";
+//        str = str.substring(5).replace('r','X').toUpperCase();
+////        str = str.substring(5).replace();
+//        System.out.println(str);
+    }
+
+
+}
